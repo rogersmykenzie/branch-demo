4d01a9066587fca57dde20160c68ecfba72881e4
Mykenzie Rogers <mykenzierogers@gmail.com>
2023-03-21 23:11:16 -0500

    add mykenzie function

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const sayName = (name) => console.log(name);
 const sayNumber = (number) => console.log(number);
 
 const sayShelby = () => console.log('Shelby Webb');
+
+const sayMykenzie = () => console.log('Mykenzie');
